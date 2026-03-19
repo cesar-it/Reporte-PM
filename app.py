0d39460d35354461d1b85a8aca805e3dc05c890e
Cesar Altamirano <154108646+cesar-it@users.noreply.github.com>
2026-03-19 16:11:34 -0500

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from collections import defaultdict
 PROJECT_KEYS = ['PM']
 
 ASSIGNEES = [
-    "Angie Tomasto", "valeria vergaray",
+    "Angie Tomasto", "Tifany Brissette Ramos Espinoza",
     "crisbel aguilar", "Miguel Carreño"
 ]
 
